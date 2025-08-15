16a64b16a0635e2b8f3fd029377bca827394bf27
Manish Karki <manish.karki@kotuko.it>
2025-08-15 07:03:01 +0545

    Remove ignored directories from repo

--- dist/config/appConfig.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appConfig = void 0;
-exports.appConfig = {
-    port: process.env.PORT,
-};
-//# sourceMappingURL=appConfig.js.map
