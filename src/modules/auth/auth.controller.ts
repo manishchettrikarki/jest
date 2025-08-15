9bf45b58ce59a88af1e5b90dcc71535c0cc8181c
Manish Karki <manish.karki@kotuko.it>
2025-08-15 07:30:41 +0545

    feat: comment to check husky precommit test

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,5 +1,5 @@
-import { Request, Response, NextFunction } from "express";
-import { IAuthService } from "./auth.service";
+import {Request, Response, NextFunction} from "express";
+import {IAuthService} from "./auth.service";
 
 /**
  *
@@ -15,15 +15,15 @@ export class AuthController {
   //
   registerUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { firstName, lastName, password } = req.body;
+      const {firstName, lastName, password} = req.body;
 
       if (!firstName || !lastName || !password) {
-        return res.status(400).json({ error: "Missing required fields" });
+        return res.status(400).json({error: "Missing required fields"});
       }
 
       const user = await this.#service.registerUser(req.body);
-      const { password: _password, ...userData } = user.get({ plain: true });
-
+      const {password: _password, ...userData} = user.get({plain: true});
+      // new comment
       return res.status(201).json({
         success: true,
         message: "User registered successfully",
